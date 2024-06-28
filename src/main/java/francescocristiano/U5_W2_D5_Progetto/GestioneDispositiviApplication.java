35f5379d19b9129231db2b6ba6904ad7a94bd524
Francesco Cristiano <frankrys97@gmail.com>
2024-06-28 10:21:07 +0200

    Create project

---
+++ src/main/java/francescocristiano/U5_W2_D5_Progetto/GestioneDispositiviApplication.java
@@ -0,0 +1,13 @@
+package francescocristiano.U5_W2_D5_Progetto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneDispositiviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneDispositiviApplication.class, args);
+	}
+
+}
---
+++ src/test/java/francescocristiano/U5_W2_D5_Progetto/GestioneDispositiviApplicationTests.java
@@ -0,0 +1,13 @@
+package francescocristiano.U5_W2_D5_Progetto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneDispositiviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
